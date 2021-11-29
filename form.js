8e0a98ffb371481ea4f06894d63e7d7cc6ee68f0
Aritra12344 <83257965+Aritra12344@users.noreply.github.com>
2021-11-29 21:17:41 +0600

    Update form.js

--- form.js
+++ form.js
@@ -35,5 +35,52 @@ alert("Signed Up");
 	}
 
 	
+	function signIn(){
+		
+		var email = document.getElementById("email");
+		var password = document.getElementById("password");
+		
+		const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+		promise.catch(e => alert(e.message));
+		
+		
+		
+		
+	}
+	
+	
+	function signOut(){
+		
+		auth.signOut();
+		alert("Signed Out");
+		
+	}
+	
+	
+	
+	auth.onAuthStateChanged(function(user){
+		
+		if(user){
+			
+			var email = user.email;
+			alert("Active User " + email);
+			
+			//Take user to a different or home page
+
+			//is signed in
+			
+		}else{
+			
+			alert("No Active User");
+			//no user is signed in
+		}
+		
+		
+		
+	});
+
+
+
+	
 
 
