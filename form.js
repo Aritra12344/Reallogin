b940e0c4db839533537b3f411dae1d8762493074
Aritra12344 <83257965+Aritra12344@users.noreply.github.com>
2021-11-29 21:54:41 +0600

    Update form.js

--- form.js
+++ form.js
@@ -1,9 +1,4 @@
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA-ZGz0GkdeyULipkALJbb0D2U85cBt1cc",
