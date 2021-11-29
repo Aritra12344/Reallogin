d285c63a079de7a66e57b79178af984a93ee33d0
Aritra12344 <83257965+Aritra12344@users.noreply.github.com>
2021-11-29 20:56:47 +0600

    Update form.js

--- form.js
+++ form.js
@@ -19,7 +19,14 @@
 	
 
 	function signUp(){
+var email = document.getElementById("email");
 
+		var password = document.getElementById("password");
+
+		
+
+const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+promise.catch(e => alert(e.message));
 		
 
 	
